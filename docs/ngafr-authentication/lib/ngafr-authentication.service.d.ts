9848cce7db88c3bad64bbc45fa7fdc445a1523e9
Rohan Dcunha <rohan.dcunha3@gmail.com>
2021-01-29 17:29:37 +0530

    feat(Angular):Update to v11

--- docs/ngafr-authentication/lib/ngafr-authentication.service.d.ts
+++ docs/ngafr-authentication/lib/ngafr-authentication.service.d.ts
@@ -2,7 +2,6 @@ import { FirebaseUISignInSuccessWithAuthResult, FirebaseUISignInFailure } from '
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { User } from '@firebase/auth-types';
-import * as ɵngcc0 from '@angular/core';
 export declare class NgafrAuthenticationService {
     private afAuth;
     private _loggedIn;
@@ -16,7 +15,4 @@ export declare class NgafrAuthenticationService {
     hideLogOutButton(hide: boolean): void;
     loginSuccessCallback(data: FirebaseUISignInSuccessWithAuthResult): void;
     loginErrorCallback(data: FirebaseUISignInFailure): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<NgafrAuthenticationService, never>;
 }
-
-//# sourceMappingURL=ngafr-authentication.service.d.ts.map
